5e64b5fa63f11fbc3e25dd0781f1f57edff9a328
Lemmy Osborne <bozhenko.rodion@mail.ru>
2022-04-10 12:51:55 +0300

    feat: add mongoose and connect it to db

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 import config from "../config/config"
 import app from "./express"
+import mongoose from "mongoose"
 
 app.listen(config.port, (err) => {
     if (err) {
         console.error(err)
     }
     console.info("Server start at port %s.", config.port)
+})
+
+mongoose.Promise = global.Promise
+mongoose.connect(config.mongoUri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
+
+mongoose.connection.on("error", () => {
+    throw new Error(`Unable to connect to database: ${config.mongoUri}`)
 })
